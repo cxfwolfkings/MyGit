641e0c6548f71a9f96fdfe7bfbb10bdc97f5259d
SOFTTEK\colin.chen <colin.chen@softtek.com>
2018-09-04 17:30:29 +0800

    队列

---
+++ DataStructure/队列/com/colin/queue/QueueTest.java
@@ -0,0 +1,15 @@
+package com.colin.queue;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class QueueTest {
+	public static void main(String[] args) {
+		LinkedList<String> linkedList =  new LinkedList<String>();
+		
+		PriorityQueue<String> priorityQueue = new PriorityQueue<String>();
+		
+		ConcurrentLinkedQueue<String> concurrentLinkedQueue = new ConcurrentLinkedQueue<String>();
+	}
+}
