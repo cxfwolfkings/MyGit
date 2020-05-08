4842e046fa7cfdf778ebb52a381abb4b17022b2e
Colin <chenxiao8516@163.com>
2020-05-08 20:07:56 +0800

    update

---
+++ DataStructure/Java/src/com/colin/ds/tree/ScNode2.java
@@ -0,0 +1,17 @@
+package com.colin.ds.tree;
+
+/**
+ * 孩子表示法
+ */
+public class ScNode2 {
+    private Object data;
+
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
