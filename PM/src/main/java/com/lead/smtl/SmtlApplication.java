b2bdff1ad29f9f4e2ed92dc563708227cae6c76a
Colin <chenxiao8516@163.com>
2020-07-27 19:03:50 +0800

    update

---
+++ PM/src/main/java/com/lead/smtl/SmtlApplication.java
@@ -0,0 +1,15 @@
+package com.lead.smtl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 提供一些附加功能
+ * @author Colin
+ */
+@SpringBootApplication
+public class SmtlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmtlApplication.class, args);
+    }
+}
---
+++ PM/src/test/java/com/lead/smtl/SmtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lead.smtl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmtlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
